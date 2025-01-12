de477ed3096d748a827d64c872c8c175388997cb
KrisQ <kquemener1@gmail.com>
2025-01-11 20:19:08 -0600

    feat: thumbnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+var allowedMimeTypes = map[string]string{
+	"image/jpeg": "jpeg",
+	"image/png":  "png",
+}
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +37,75 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not parse form", err)
+		return
+	}
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not find file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not parse media type", err)
+		return
+	}
+	extension, allowed := allowedMimeTypes[mediaType]
+	if !allowed {
+		respondWithError(w, http.StatusBadRequest, "Unsupported file type. Please upload JPG, PNG, or GIF", nil)
+		return
+	}
+
+	err = os.MkdirAll(cfg.assetsRoot, 0755)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create assets directory", err)
+		return
+	}
+
+	fileName := fmt.Sprintf("%v.%v", videoID, extension)
+	path := filepath.Join(cfg.assetsRoot, fileName)
+
+	dst, err := os.Create(path)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create file", err)
+		return
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not write file", err)
+		return
+	}
+
+	dataUrl := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoID, extension)
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "video not found", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized", err)
+		return
+	}
+
+	video.ThumbnailURL = &dataUrl
+	err = cfg.db.UpdateVideo(video)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not update video", err)
+		return
+	}
 
 	respondWithJSON(w, http.StatusOK, struct{}{})
 }
